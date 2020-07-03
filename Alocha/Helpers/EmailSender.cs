e508266597ea5699937a523d4e054079885bb896
Kamil Grabowski <kaamilgrabowski1@wp.pl>
2020-07-03 08:58:19 +0200

    Add helpers EmailSender and RandomPasswordGenerator

---
+++ Alocha/Helpers/EmailSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Alocha.WebUi.Helpers
+{
+    public class EmailSender
+    {
+        public static async Task<bool> MailSender(string userEmail, string subject, string body)
+        {
+            MailMessage message = new MailMessage();
+
+            message.To.Add(userEmail);
+            message.From = new MailAddress("programista2020@onet.pl", "Aplikacja Ebok");
+            message.IsBodyHtml = true;
+            message.Subject = subject;
+            message.Body = body;
+
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "programista2020@onet.pl",
+                    Password = "Juniordeveloper1"
+                };
+
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.poczta.onet.pl";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                await smtp.SendMailAsync(message);
+                return true;
+
+            }
+        }
+    }
+}
---
+++ Alocha/Helpers/RandomPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Alocha.WebUi.Helpers
+{
+    public class RandomPassword
+    {
+        public static string GenerateToken(int length = 15)
+        {
+            using (RNGCryptoServiceProvider cryptRNG = new RNGCryptoServiceProvider())
+            {
+                byte[] tokenBuffer = new byte[length];
+                cryptRNG.GetBytes(tokenBuffer);
+                return Convert.ToBase64String(tokenBuffer);
+            }
+        }
+    }
+}
