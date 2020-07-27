15665cf2f9261bf6c65e89588ed1c105b55ad389
Kamil Grabowski <kaamilgrabowski1@wp.pl>
2020-07-27 15:42:14 +0200

    Adding LogerInformation in WorkerServices.

--- Alocha/Services/WorkerServices.cs
+++ Alocha/Services/WorkerServices.cs
@@ -35,52 +35,56 @@ namespace Alocha.WebUi.Services
 
         private void DoWork(object state)
         {
-            var tomorrowDate = DateTime.Today.AddDays(1);
-            var list = new List<string>();
-            using (var scope = _services.CreateScope())
+            _logger.LogInformation("Worker Service method DoWork entrance.");
+            if (DateTime.Now.Hour >= 10 && DateTime.Now.Hour <= 14)
             {
-                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                var sows = context.Sow.Where(s => !s.IsRemoved && s.DateBorn == tomorrowDate && !s.IsSmsSend
-                        || !s.IsRemoved && s.DateDetachment == tomorrowDate && !s.IsSmsSend
-                        || !s.IsRemoved && s.DateInsimination == tomorrowDate && !s.IsSmsSend
-                        || !s.IsRemoved && s.VaccineDate == tomorrowDate && !s.IsSmsSend).ToList();
-                foreach (var item in sows)
+                var tomorrowDate = DateTime.Today.AddDays(1);
+                var list = new List<string>();
+                using (var scope = _services.CreateScope())
                 {
-                    if (item.User.PhoneNumberConfirmed)//If number is confirm 
+                    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    var sows = context.Sow.Where(s => !s.IsRemoved && s.DateBorn == tomorrowDate && !s.IsSmsSend
+                            || !s.IsRemoved && s.DateDetachment == tomorrowDate && !s.IsSmsSend
+                            || !s.IsRemoved && s.DateInsimination == tomorrowDate && !s.IsSmsSend
+                            || !s.IsRemoved && s.VaccineDate == tomorrowDate && !s.IsSmsSend).ToList();
+
+                    foreach (var item in sows)
                     {
-                        if (!list.Contains(item.UserId))
+                        if (item.User.PhoneNumberConfirmed)
                         {
-                            list.Add(item.UserId);
-                            var userSows = sows.Where(u => u.UserId == item.UserId);
-                            var message = "Aplikacja Alocha.\nJutrzejsze wydarzenia:";
-                            foreach (var sow in userSows)
+                            if (!list.Contains(item.UserId))
                             {
-                                string status = "";
-                                switch (sow.Status)
+                                list.Add(item.UserId);
+                                var userSows = sows.Where(u => u.UserId == item.UserId);
+                                var message = "Aplikacja Alocha.\nJutrzejsze wydarzenia:";
+                                foreach (var sow in userSows)
                                 {
-                                    case "Prośna":
-                                        status = "Poród";
-                                        if (sow.VaccineDate == tomorrowDate)
-                                            status = "Szczepienie";
-                                        break;
-                                    case "Luźna":
-                                        status = "Inseminacja";
-                                        break;
-                                    case "Laktacja":
-                                        status = "Oderwanie prosiąt";
-                                        break;
+                                    string status = "";
+                                    switch (sow.Status)
+                                    {
+                                        case "Prośna":
+                                            status = "Poród";
+                                            if (sow.VaccineDate == tomorrowDate)
+                                                status = "Szczepienie";
+                                            break;
+                                        case "Luźna":
+                                            status = "Inseminacja";
+                                            break;
+                                        case "Laktacja":
+                                            status = "Oderwanie prosiąt";
+                                            break;
+                                    }
+                                    message += string.Format("\nLocha nr {0} - {1}", sow.Number, status);
+                                    sow.IsSmsSend = true;
+                                    context.SaveChanges();
                                 }
-                                message += string.Format("\nLocha nr {0} - {1}", sow.Number, status);
-                                sow.IsSmsSend = true;
-                                context.SaveChanges();
+                                SmsSender.SendSmsAsync(string.Format("48{0}", item.User.PhoneNumber), message).GetAwaiter().GetResult();
+                                _logger.LogInformation(string.Format("The worker service sends a sms to user: {0}, on the number {1}", item.User.Email, item.User.PhoneNumber));
                             }
-                            SmsSender.SendSmsAsync(string.Format("48{0}", item.User.PhoneNumber), message).GetAwaiter().GetResult();
-                            _logger.LogInformation("Wysłano sms pod numer ");
-                               
                         }
-                    }
-                };
-            }            
+                    };
+                }
+            }
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
