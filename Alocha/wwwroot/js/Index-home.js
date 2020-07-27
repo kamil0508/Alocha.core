10f38b39aa2c1b4d709b8aa7f254e94762c41bb5
Kamil Grabowski <kaamilgrabowski1@wp.pl>
2020-07-27 18:13:28 +0200

    Adding Index-Home script in the Index view of the controller Home.

---
+++ Alocha/wwwroot/js/Index-home.js
@@ -0,0 +1,33 @@
+﻿window.onload = function () {
+    $.ajax({
+        type: "GET",
+        url: "/Home/UpcomingTaskCount",
+        contentType: "application/json; charset=utf-8",
+        dataType: "text",
+        success: function (count) {
+            if (count != "0") {
+                alertify.set("notifier", "delay", 10);
+                var msg = alertify.error("Ilość najblirzszych zdarzeń: " + count);
+                msg.callback = function (isClicked) {
+                    if (isClicked)
+                        window.location.href = "/Home/UpcomingTask";
+                };
+            }
+            else {
+                alertify.set("notifier", "delay", 10);
+                var msg = alertify.success("Nie ma nadchodzących zdarzeń w najbliższym tygodniu");
+                msg.callback = function (isClicked) {
+                    if (isClicked)
+                        window.location.href = "/Home/UpcomingTask";
+                };
+            }
+        },
+        error: function () {
+            var msg = alertify.success("Loading...");
+            msg.callback = function (isClicked) {
+                if (isClicked)
+                    window.location.href = "/Home/UpcomingTask";
+            };
+        }
+    });
+}
