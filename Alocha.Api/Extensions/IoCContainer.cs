cbcc0a63a44a165744a85dfa2b4ab5e60c670417
Kamil Grabowski <kaamilgrabowski1@wp.pl>
2020-08-06 12:28:39 +0200

    Adding IoC Container and inject ApplicationDbContext in ConfigureServices method.

---
+++ Alocha.Api/Extensions/IoCContainer.cs
@@ -0,0 +1,27 @@
+﻿using Alocha.Domain.Interfaces;
+using Alocha.Domain.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Alocha.Api.Extensions
+{
+    public static class IoCContainer
+    {
+        public static IServiceCollection RepositoryInjector(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+        public static IServiceCollection ServiceInjetor(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
