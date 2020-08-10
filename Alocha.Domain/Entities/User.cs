591bec1ff36fa037508c8253dd9423b838d3ae62
Kamil Grabowski <kaamilgrabowski1@wp.pl>
2020-08-10 14:25:55 +0200

    Adding migration, extend User table by RefreshToken column.

--- Alocha.Domain/Entities/User.cs
+++ Alocha.Domain/Entities/User.cs
@@ -7,6 +7,8 @@ namespace Alocha.Domain.Entities
 {
     public class User : IdentityUser 
     {
+        public string RefreshToken { get; set; }
+
         public virtual ICollection<Sow> Sows { get; set; }
     }
 }
